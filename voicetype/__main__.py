7b40105a7348b1b32a9d3e44ec0257af26d19bcc
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-04-27 09:13:47 -0500

    chore: Remove uvicorn run command from main

--- voicetype/__main__.py
+++ voicetype/__main__.py
@@ -2,12 +2,6 @@ import uvicorn
 
 
 def main():
-    uvicorn.run(
-        "voicetype.app:app",
-        host="0.0.0.0",
-        port=8000,
-        # reload=args.test,
-    )
 
 if __name__ == "__main__":
     main()
