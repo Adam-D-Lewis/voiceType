d66e3ab9ab0c7a796d32ea6078690ba053da7c0a
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-04-27 20:53:28 -0500

    fix bug with play_audio

--- voicetype/utils.py
+++ voicetype/utils.py
@@ -23,38 +23,39 @@ def type_text(text):
 
 def play_audio(filename):
     # Open the file for reading.
-    wav = wave.open(filename, 'rb')
+    with filename.open('rb') as f:
+        wav = wave.open(f, 'rb')
 
-    # Create an interface to PortAudio.
-    pa = pyaudio.PyAudio()
+        # Create an interface to PortAudio.
+        pa = pyaudio.PyAudio()
 
-    # Open a .Stream object to write the WAV file to.
-    # 'output = True' indicates that the sound will be outputted to the speaker.
-    # 'rate' is the sample rate from the wav file, which we set as the rate of the output stream.
-    # 'channels' is also set using the method getnchannels().
-    # 'format' we get from a lookup dictionary provided by PyAudio containing the appropriate format for the sample width returned from the wave file.
-    stream = pa.open(
-        format = pa.get_format_from_width(wav.getsampwidth()),
-        channels = wav.getnchannels(),
-        rate = wav.getframerate(),
-        output = True
-    )
+        # Open a .Stream object to write the WAV file to.
+        # 'output = True' indicates that the sound will be outputted to the speaker.
+        # 'rate' is the sample rate from the wav file, which we set as the rate of the output stream.
+        # 'channels' is also set using the method getnchannels().
+        # 'format' we get from a lookup dictionary provided by PyAudio containing the appropriate format for the sample width returned from the wave file.
+        stream = pa.open(
+            format = pa.get_format_from_width(wav.getsampwidth()),
+            channels = wav.getnchannels(),
+            rate = wav.getframerate(),
+            output = True
+        )
 
-    # CHUNK size
-    CHUNK = 1024
+        # CHUNK size
+        CHUNK = 1024
 
-    # Read data in chunks
-    data = wav.readframes(CHUNK)
-
-    # Play the sound by writing the audio data to the stream.
-    while data != b'':
-        stream.write(data)
+        # Read data in chunks
         data = wav.readframes(CHUNK)
 
-    # Close and terminate everything properly.
-    stream.stop_stream()
-    stream.close()
-    pa.terminate()
+        # Play the sound by writing the audio data to the stream.
+        while data != b'':
+            stream.write(data)
+            data = wav.readframes(CHUNK)
+
+        # Close and terminate everything properly.
+        stream.stop_stream()
+        stream.close()
+        pa.terminate()
 
 def make_sound_thread(filepath):
     sound_file_path = str(filepath)
